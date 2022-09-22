02768c418f22175a7936236e18465d2f8797c8bc
ebo-visio5 <kenvicbernard17@mail.com>
2022-09-22 21:10:45 +0100

    students_grades

---
+++ index.js
@@ -0,0 +1,84 @@
+// This is a conditional expression that is used to print out students grades.
+
+let marks = (70)
+
+{
+if (marks >=90)
+console.log('Grade AA');
+else
+if (marks >80 && marks <=90)
+{
+console.log('Grade AB');
+}
+else
+if (marks >70 && marks <=80)
+{
+console.log('Grade BB');
+}
+else
+if (marks >60 && marks <=70)
+{
+console.log('Grade BC');
+}
+else
+if (marks >50 && marks <=60)
+{
+console.log('Grade CC');
+}
+else 
+if (marks >40 && marks <=50)
+{
+console.log('Grade CD');
+}
+else
+if (marks >30 && marks <=40)
+{
+console.log('Grade DD');
+}
+else
+console.log('Grade FF');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // This is a conditional expression that is used to print out students grades.
+
+// let marks = (100)
+
+// let grade =
+
+
+//  (marks >=90)('Grade AA');
+
+//  (marks >80 && marks <=90)('Grade AB');
+
+
+//  (marks >70 && marks <=80)('Grade BB');
+
+
+//  (marks >60 && marks <=70)('Grade BC');
+
+
+//  (marks >50 && marks <=60)('Grade CC');
+
+ 
+//  (marks >40 && marks <=50)('Grade CD');
+
+//  (marks >30 && marks <=40)('Grade DD');
+// ('Grade FF');
+// console.log(grade);
